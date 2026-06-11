89597d0d69b27df6a5b1aef673a8672094236e29
olinll <olinl@foxmail.com>
2026-06-11 21:47:47 +0800

    chore: 更新图标命名、图床标识及依赖
    - 图标文件改为 kebab-case 命名
    - BlogTech 图片存储改为 Aliyun OSS
    - 添加 @iconify-json/tabler 依赖

--- app/components/widget/BlogTech.vue
+++ app/components/widget/BlogTech.vue
@@ -1,58 +1,59 @@
 <script setup lang="ts">
-import { merge } from "es-toolkit/object";
-import { packageManager, version } from "~~/package.json";
-import pnpmWorkspace from "~~/pnpm-workspace.yaml";
-import { Icon } from "#components";
+import { merge } from 'es-toolkit/object'
+import { packageManager, version } from '~~/package.json'
+import pnpmWorkspace from '~~/pnpm-workspace.yaml'
+import { Icon } from '#components'
 
-const appConfig = useAppConfig();
+const appConfig = useAppConfig()
 const {
 	public: { arch, ci, nodeVersion, platform },
-} = useRuntimeConfig();
+} = useRuntimeConfig()
 
 const ciPlatform = computed(() => {
-	const iconName = ciIcons[ci];
-	if (!iconName) return "";
+	const iconName = ciIcons[ci]
+	if (!iconName)
+		return ''
 
-	const iconNode = iconName.startsWith("http")
-		? h("img", { src: iconName, alt: "" })
-		: h(Icon, { name: iconName });
+	const iconNode = iconName.startsWith('http')
+		? h('img', { src: iconName, alt: '' })
+		: h(Icon, { name: iconName })
 
-	return h("span", {}, [iconNode, ` ${ci.split(" ")[0]}`]);
-});
+	return h('span', {}, [iconNode, ` ${ci.split(' ')[0]}`])
+})
 
 // @ts-expect-error pnpm-workspace.yaml 无类型定义
-const packages = merge(...Object.values(pnpmWorkspace.catalogs));
-const [pm, pmVersion] = packageManager.split("@") as [string, string];
+const packages = merge(...Object.values(pnpmWorkspace.catalogs))
+const [pm, pmVersion] = packageManager.split('@') as [string, string]
 
 const service = computed(() => [
-	...(ci ? [{ label: "构建平台", value: ciPlatform }] : []),
-	// { label: '图片存储', value: () => [h(Icon, { name: 'devicon:cloudflare' }), ' R2'] },
-	{ label: "软件协议", value: "MIT" },
-	{ label: "文章许可", value: appConfig.copyright.abbr },
-	{ label: "规范域名", value: getDomain(appConfig.url) },
-]);
+	...(ci ? [{ label: '构建平台', value: ciPlatform }] : []),
+	{ label: '图片存储', value: () => [h(Icon, { name: 'simple-icons:alibabacloud' }), ' OSS'] },
+	{ label: '软件协议', value: 'MIT' },
+	{ label: '文章许可', value: appConfig.copyright.abbr },
+	{ label: '规范域名', value: getDomain(appConfig.url) },
+])
 
 const techstack = computed(() => [
-	{ label: "Blog", value: version },
-	{ label: "Vue", value: packages.vue },
-	{ label: "Nuxt", value: packages.nuxt },
-	{ label: "Content", value: packages["@nuxt/content"] },
-	{ label: "Node", value: nodeVersion },
+	{ label: 'Blog', value: version },
+	{ label: 'Vue', value: packages.vue },
+	{ label: 'Nuxt', value: packages.nuxt },
+	{ label: 'Content', value: packages['@nuxt/content'] },
+	{ label: 'Node', value: nodeVersion },
 	{ label: pm, value: pmVersion },
-	{ label: "OS", value: platform },
-	{ label: "Arch", value: arch },
-]);
+	{ label: 'OS', value: platform },
+	{ label: 'Arch', value: arch },
+])
 
-const expand = ref(false);
+const expand = ref(false)
 </script>
 
 <template>
-	<BlogWidget card grayscale title="技术信息">
-		<ZDlGroup :items="service" />
-		<ZExpand v-model="expand" in-place name="构建信息">
-			<ZDlGroup size="small" :items="techstack" />
-		</ZExpand>
-	</BlogWidget>
+<BlogWidget card grayscale title="技术信息">
+	<ZDlGroup :items="service" />
+	<ZExpand v-model="expand" in-place name="构建信息">
+		<ZDlGroup size="small" :items="techstack" />
+	</ZExpand>
+</BlogWidget>
 </template>
 
 <style lang="scss" scoped>
